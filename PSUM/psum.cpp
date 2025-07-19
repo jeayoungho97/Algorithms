6adeba74b35ef7be6f8ac8cc0c2be565a504be54
제영호 <jeayh97@naver.com>
2025-07-19 10:38:40 +0900

    [add] solved

---
+++ PSUM/psum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+  int psum[100001];
+  psum[0] = 0;
+  for (int i = 1; i <= n; ++i) {
+    int num;
+    std::cin >> num;
+    psum[i] = psum[i - 1] + num;
+  }
+
+  std::vector<int> ret;
+  for (int i = 0 ; i < m; ++i) {
+    int start, end;
+    std::cin >> start >> end;
+    ret.push_back(psum[end] - psum[start - 1]);
+  }
+
+  for (int i : ret) std::cout << i << '\n';
+
+  return 0;
+}
