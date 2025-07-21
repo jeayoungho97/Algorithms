f39fe36e976af677580f95137cbe5f956736e945
제영호 <jeayh97@naver.com>
2025-07-21 21:23:48 +0900

    [add] solved

---
+++ math/permutation_without_rep.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int n, m;
+std::vector<int> input, list, visited;
+
+void go() {
+  if (list.size() == m) {
+    for (int i : list) std::cout << i << ' ';
+    std::cout << '\n';
+    return;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    if (visited[i]) continue;
+    list.push_back(input[i]);
+    visited[i] = 1;
+    go();
+    visited[i] = 0;
+    list.pop_back();
+  }
+}
+
+int main() {
+  std::cin >> n >> m;
+  input.resize(n);
+  visited.resize(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> input[i];
+  }
+  std::sort(input.begin(), input.end());
+  go();
+
+  return 0;
+}
