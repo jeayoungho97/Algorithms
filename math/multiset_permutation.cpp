5888e90d1036dcb03b2c64fcea4b378e23a284f6
제영호 <jeayh97@naver.com>
2025-07-21 21:12:44 +0900

    [add] solved

---
+++ math/multiset_permutation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int n, m;
+std::vector<int> list;
+
+void go(int here) {
+  if (list.size() == m) {
+    for (int i : list) std::cout << i << ' ';
+    std::cout << '\n';
+    return;
+  }
+
+  for (int i = here; i <= n; ++i) {
+    list.push_back(i);
+    go(i);
+    list.pop_back();
+  }
+}
+
+int main() {
+  std::cin >> n >> m;
+  go(1);
+  return 0;
+}
