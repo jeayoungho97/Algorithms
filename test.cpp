256f4ade2004ad95be3dd1b2ae27df7fb54c3e19
jeayh97 <jeayh97@naver.com>
2025-08-05 17:55:09 +0900

    최신화

--- test.cpp
+++ test.cpp
@@ -1,116 +1,39 @@
+// 도형 끼우기
+// 모양은 방향으로 저장하기 => ex) shape = {1,2,1} (오, 아, 오)
+// 위의 예시 모양을 90도 돌리면 => shape_90 = {2,3,2} (아, 왼, 아)
+// 즉 1 씩 더하고 4로 나눈 나머지
+// 모양이 중복되는 경우는 두 가지 => 90도 돌려서 같으면 첫번째 shape 만 쓰면 됨.
+//                            => 90도는 다르지만 180도 돌려서 같으면 첫번째, 두번째 shape 만 쓰면 됨.
+// 돌려서 같은 모양인지 어떻게 알지..?
+
+// 이건 안됨,,, 모양이 가지 없이 한붓그리기 가능한 경우에만 가능
+// 모양 그 자체를 써야할듯?
+
+
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-const int MAX = 987654321;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
 
-const int change[8][4] = {
-  {0,1,2,3},
-  {4,1,2,3},
-  {4,1,2,3},
-  {4,5,2,3},
-  {4,5,2,3},
-  {4,5,6,3},
-  {4,5,6,3},
-  {4,5,6,3},
-};
+char shape[4][4], map[10][10];
 
-struct apple
-{
-  int y, x, order;
-  bool operator<(apple right) {
-    return order < right.order;
-  }
-};
-
-void go(std::vector<apple> apples, std::vector<std::vector<int>> &val)
-{
-  for (int i = 0; i + 1 < apples.size(); ++i) {
-    int ny = apples[i + 1].y;
-    int nx = apples[i + 1].x;
-
-    int cy = apples[i].y;
-    int cx = apples[i].x;
-
-    int area;
-    if (ny < cy) {
-      if (nx > cx) area = 1;
-      else if (nx < cx) area = 7;
-      else area = 0;
-    }
-    else if (ny > cy) {
-      if (nx > cx) area = 3;
-      else if (nx < cx) area = 5;
-      else area = 4;
-    }
-    else {
-      if (nx > cx) area = 2;
-      else if (nx < cx) area = 6;
-    }
-
-    if (i == 0) {
-      int dir = 1;
-      area = (area + 6 * dir) % 8;
-      for (int j = 0; j < 4; ++j) {
-        val[i + 1][j] = change[area][(j + (4 - dir)) % 4];
-      }
-    }
-    else {
-      for (int dir = 0; dir < 4; ++dir) {
-        area = (area + 6 * dir) % 8;
+int main() {
+    for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
-          val[i + 1][j] = std::min(val[i + 1][j], change[area][(j + (4 - dir)) % 4]);
+            std::cin >> shape[i][j];
         }
-      }
     }
 
-  }
-}
-
-int solve()
-{
-  int n;
-  std::cin >> n;
-
-  std::vector<apple> apples;
-  apples.push_back({0, 0, 0});
-
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
-      int temp;
-      std::cin >> temp;
-      if (temp != 0)
-      {
-        apples.push_back({i, j, temp});
-      }
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            std::cin >> map[i][j];
+        }
     }
-  }
-  std::sort(apples.begin(), apples.end());
 
-  std::vector<std::vector<int>> val(apples.size() + 1, std::vector<int>(4, MAX));
-  // val[i][j] = i번째 점이 j 방향이 되기 위해서 필요한 최소한의 우회전의 수
+    for
 
-  go(apples, val);
 
-  int ret = MAX;
-  for (int i : val[apples.size() - 1]) {
-    ret = std::min(ret, i);
-  }
-
-  return ret;
-}
-
-int main()
-{
-  int t;
-  std::cin >> t;
-  int cnt = 1;
-  while (t--)
-  {
-    std::cout << '#' << cnt++ << ' ' << solve() << '\n';
-    ;
-  }
-  return 0;
+    return 0;
 }
