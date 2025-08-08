ed43bcdf2c145099ba1514d675d5288103ce120d
jeayh97 <jeayh97@naver.com>
2025-08-08 17:50:10 +0900

    최신화

--- test.cpp
+++ test.cpp
@@ -1,5 +1,30 @@
+// 두 로봇
+// 그냥 DFS 로 이어서
+// 가면서 있었던 경로 중에서 가장 긴 놈을 빼면 답임
+
 #include <iostream>
+#include <vector>
+
+int n, a, b, ans;
+std::vector<std::vector<int>> adj;
+
+void go() {
+    
+}
 
 int main() {
+    std::cin >> n >> a >> b;
+
+    adj.resize(n, std::vector<int>(n, 0));
+
+    for (int i = 0; i < n - 1; ++i) {
+        int x, y, l;
+
+        std::cin >> x >> y >> l;
+        adj[x][y] = adj[y][x] = l;
+    }
+
+    go();
+
     return 0;
 }
