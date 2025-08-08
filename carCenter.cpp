87c3981d5d0fe9dde55a2a8f466e5f72bb18bfb1
jeayh97 <jeayh97@naver.com>
2025-08-08 11:20:30 +0900

    최신화

---
+++ carCenter.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int customer_num, target_a, target_b;
+int T, ans;
+std::vector<int> a, b, eta;
+
+void init() {
+    int n, m, k, a, b;
+}
+
+int main() {
+    std::cin >> T;
+    for (int tc = 1; tc <= T; ++tc) {
+        init();
+
+
+
+
+        std::cout << '#' << tc << ' ' << ans << '\n';
+    }    
+
+
+
+    return 0;
+}
