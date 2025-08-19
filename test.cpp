d441c5ca5ae67f0e749e5ab097fdc2231568f1ac
jeayh97 <jeayh97@naver.com>
2025-08-19 18:00:06 +0900

    최신화

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-<<<<<<< HEAD
 #include <algorithm>
 #define min(a,b) (a>b?b:a)
 #define max(a,b) (a>b?a:b)
@@ -46,56 +45,4 @@ int main() {
         }
     }
     return 0;
-=======
-#include <cstring>
-
-int n, ans;
-int dp[1 << 10][20000];
-std::vector<int> arr;
-
-void init() {
-  ans = 0;
-  arr.clear();
-  std::memset(dp, -1, sizeof(dp));
-}
-
-int go(int bitmask, int diff) {
-  if (diff < 0) return 0;
-  if (bitmask == (1 << n) - 1) return 1;
-
-  int &ret = dp[bitmask][diff + 10000];
-  if (ret != -1) return ret;
-
-  ret = 0;
-  for (int i = 0; i < n; ++i) {
-    if (bitmask & (1 << i)) continue;
-    ret += go(bitmask | (1 << i), diff + arr[i]);
-    ret += go(bitmask | (1 << i), diff - arr[i]);
   }
-  return ret;
-}
-
-int main() {
-  int t;
-  std::cin >> t;
-
-  for (int tc = 1; tc <= t; ++tc) {
-    init();
-
-    std::cin >> n;
-    
-    for (int i = 0; i < n; ++i) {
-      int temp;
-      std::cin >> temp;
-      arr.push_back(temp);
-    }
-
-    ans = go(0, 0);
-    std::cout << '#' << tc << ' ' << ans << '\n';
-  }
-
-
-
-  return 0;
->>>>>>> 40087864985c935d3837f4db830ebbcb71f7ca5d
-}
