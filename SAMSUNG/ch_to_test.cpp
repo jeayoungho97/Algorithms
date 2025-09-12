9267e54f744b09fed4a6527dc33ab8eafae3830e
jeayoungho97 <jeayh97@naver.com>
2025-09-12 14:18:20 +0900

    [solved] 미생물 연구 (코드트리 삼성 기출 2025 상반기 오후 1번)

--- SAMSUNG/ch_to_test.cpp
+++ SAMSUNG/ch_to_test.cpp
@@ -16,7 +16,7 @@ struct GROUP {
                                 // 있는지 (즉 모양을 결정) -> 값은 num 과 같음
   bool isAlive = true;
   bool operator<(const GROUP &rhs) const {
-    if (size == rhs.size) return num < rhs.num;
+    if (size == rhs.size) return num > rhs.num;
     return size < rhs.size;
   }
 };
@@ -58,12 +58,12 @@ int bfs(int y, int x, int map[MAX_SIZE][MAX_SIZE]) {
 
 int main() {
   cin >> n >> q;
-
   vector<GROUP> groups;  // q 개의 미생물 군집 (1번 인덱스부터 시작)
   groups.push_back({});  // dummy node 추가
   groups[0].isAlive = false;
 
   for (int query = 1; query <= q; ++query) {
+    // cout << "\n==============START " << query << "===============\n";
     int ans = 0;
 
     int r1, c1, r2, c2;
@@ -81,25 +81,25 @@ int main() {
     }
 
     // DB
-    cout << "\ncurrent MAP\n";
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        cout << MAP[i][j] << ' ';
-      }
-      cout << '\n';
-    }
+    // cout << "\ncurrent MAP\n";
+    // for (int i = 0; i < n; ++i) {
+    //   for (int j = 0; j < n; ++j) {
+    //     cout << MAP[i][j] << ' ';
+    //   }
+    //   cout << '\n';
+    // }
 
-    cout << "\nGROUP to put in\n";
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        if (i >= c1 && i < c2 && j >= r1 && j < r2)
-          cout << query << ' ';
-        else
-          cout << 0 << ' ';
-      }
-      cout << '\n';
-    }
-    cout << '\n';
+    // cout << "\nGROUP to put in\n";
+    // for (int i = 0; i < n; ++i) {
+    //   for (int j = 0; j < n; ++j) {
+    //     if (i >= c1 && i < c2 && j >= r1 && j < r2)
+    //       cout << query << ' ';
+    //     else
+    //       cout << 0 << ' ';
+    //   }
+    //   cout << '\n';
+    // }
+    // cout << '\n';
     // DB
 
     // 기존 맵에 다른 미생물 있다면 삭제
@@ -113,34 +113,37 @@ int main() {
           now.map[row - cy][col - cx] = 0;
           if (!--now.size) now.isAlive = false;
 
-          // for debugging
-          cout << '\n';
-          for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-              cout << now.map[i][j] << ' ';
-            }
-            cout << '\n';
-          }
-          cout << '\n';
+          // DB
+          // cout << '\n';
+          // for (int i = 0; i < n; ++i) {
+          //   for (int j = 0; j < n; ++j) {
+          //     cout << now.map[i][j] << ' ';
+          //   }
+          //   cout << '\n';
+          // }
+          // cout << '\n';
 
-          cout << "group :" << num_group << ", cy : " << cy << ", cx : " << cx
-               << '\n';
-          cout << "row - cy : " << row - cy << ", col - cx : " << col - cx
-               << '\n';
+          // cout << "group :" << num_group << ", cy : " << cy << ", cx : " <<
+          // cx
+          //      << '\n';
+          // cout << "row - cy : " << row - cy << ", col - cx : " << col - cx
+          //      << '\n';
+          // DB
+
           MAP[row][col] = 0;
         }
       }
     }
 
     // DB
-    cout << "MAP after erasing groups\n";
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        cout << MAP[i][j] << ' ';
-      }
-      cout << '\n';
-    }
-    cout << '\n';
+    // cout << "MAP after erasing groups\n";
+    // for (int i = 0; i < n; ++i) {
+    //   for (int j = 0; j < n; ++j) {
+    //     cout << MAP[i][j] << ' ';
+    //   }
+    //   cout << '\n';
+    // }
+    // cout << '\n';
     // DB
 
     // 각 군집의 map에서 살아있는 점 하나 잡고 플러드필 돌림
@@ -149,14 +152,16 @@ int main() {
     for (GROUP &now : groups) {
       if (!now.isAlive) continue;
 
-      cout << '\n';
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-          cout << now.map[i][j] << ' ';
-        }
-        cout << '\n';
-      }
-      cout << '\n';
+      // DB
+      // cout << '\n';
+      // for (int i = 0; i < n; ++i) {
+      //   for (int j = 0; j < n; ++j) {
+      //     cout << now.map[i][j] << ' ';
+      //   }
+      //   cout << '\n';
+      // }
+      // cout << '\n';
+      // DB
 
       // 삭제된 미생물 군집들의 map 재조정
       int sy = 20, sx = 20;
@@ -171,11 +176,12 @@ int main() {
       int temp_map[MAX_SIZE][MAX_SIZE];
       for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-          if (now.map[i][j]) {
-            now.map[i - sy][j - sx] = now.map[i][j];
-            // 여기가 틀림ㅋ
-            
-          }
+          temp_map[i - sy][j - sx] = (now.map[i][j]) ? now.map[i][j] : 0;
+        }
+      }
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          now.map[i][j] = temp_map[i][j];
         }
       }
 
@@ -189,17 +195,17 @@ int main() {
             cnt = bfs(i, j, now.map);
 
             // DB
-            cout << "\nGROUP to do BFS\n";
-            for (int i = 0; i < n; ++i) {
-              for (int j = 0; j < n; ++j) {
-                cout << now.map[i][j] << ' ';
-              }
-              cout << '\n';
-            }
-            cout << '\n';
+            // cout << "\nGROUP to do BFS\n";
+            // for (int i = 0; i < n; ++i) {
+            //   for (int j = 0; j < n; ++j) {
+            //     cout << now.map[i][j] << ' ';
+            //   }
+            //   cout << '\n';
+            // }
+            // cout << '\n';
 
-            cout << now.num << "의 넓이는 : " << now.size << '\n';
-            cout << "플러드필 결과는 : " << cnt << '\n';
+            // cout << now.num << "의 넓이는 : " << now.size << '\n';
+            // cout << "플러드필 결과는 : " << cnt << '\n';
             // DB
 
             breaker = true;
@@ -223,89 +229,106 @@ int main() {
       sorted_groups.push(now);
     }
 
+    // cout << "order : ";
     while (!sorted_groups.empty()) {
       int group_num = sorted_groups.top().num;
       sorted_groups.pop();
       GROUP &now = groups[group_num];
+
+      // cout << now.num << ' ';
 
       // 모든 좌표를 돌면서 군집이 들어갈 수 있는지 확인
       bool found = false;
       for (int j = 0; j < n; ++j) {
         if (found) break;
         for (int i = 0; i < n; ++i) {
-          if (MAP[i][j]) continue;
 
           bool good_to_go = true;
+
           for (int row = 0; row < n; ++row) {
             if (!good_to_go) break;
             for (int col = 0; col < n; ++col) {
+              if (!now.map[row][col]) continue;
               int ny = i + row;
               int nx = j + col;
-              if ((ny < 0 || nx < 0 || ny >= n || nx >= n || MAP[ny][nx]) &&
-                  now.map[row][col]) {
+              if (ny < 0 || nx < 0 || ny >= n || nx >= n) {
+                // if (query == 37) {
+                //   cout << "\ni = " << i << ", j = " << j << "\nny = " << ny << ", nx = " << nx << " and passed because out of bounds\n";
+                // }
+                good_to_go = false;
+                break;
+              }
+
+              if (MAP[ny][nx]) {
+                // if (query == 37) {
+                //   cout << "\ni = " << i << ", j = " << j << "\nny = " << ny << ", nx = " << nx << " and passed because MAP overlap\n";
+                // }
                 good_to_go = false;
                 break;
               }
             }
           }
 
-          // 들어갈 수 있다면 집어넣기
-          if (good_to_go) {
-            now.cy = i, now.cx = j;
+        // 들어갈 수 있다면 집어넣기
+        if (good_to_go) {
+          now.cy = i, now.cx = j;
 
-            // 인접한 군집 확인용 visited
-            bool visited_others[51] = {0};
-            visited_others[now.num] = true;
+          // 인접한 군집 확인용 visited
+          bool visited_others[51] = {0};
+          visited_others[now.num] = true;
 
-            for (int row = 0; row < n; ++row) {
-              for (int col = 0; col < n; ++col) {
-                int ny = i + row;
-                int nx = j + col;
-                if (now.map[row][col]) {
-                  MAP[ny][nx] = now.num;
+          for (int row = 0; row < n; ++row) {
+            for (int col = 0; col < n; ++col) {
+              int ny = i + row;
+              int nx = j + col;
+              if (now.map[row][col]) {
+                MAP[ny][nx] = now.num;
 
-                  // 4방향으로 돌면서 인접한 군집이 있는지 확인
-                  for (int dir = 0; dir < 4; ++dir) {
-                    int oy = ny + dy[dir];
-                    int ox = nx + dx[dir];
-                    if (oy < 0 || ox < 0 || oy >= n || ox >= n) continue;
-                    if (!MAP[oy][ox]) continue;
-                    if (visited_others[MAP[oy][ox]]) continue;
+                // 4방향으로 돌면서 인접한 군집이 있는지 확인
+                for (int dir = 0; dir < 4; ++dir) {
+                  int oy = ny + dy[dir];
+                  int ox = nx + dx[dir];
+                  if (oy < 0 || ox < 0 || oy >= n || ox >= n) continue;
+                  if (!MAP[oy][ox]) continue;
+                  if (visited_others[MAP[oy][ox]]) continue;
 
-                    visited_others[MAP[oy][ox]] = true;
-                  }
+                  visited_others[MAP[oy][ox]] = true;
                 }
               }
             }
-
-            // 인접한 군집끼리 계산
-            for (int i = 1; i < groups.size(); ++i) {
-              if (!groups[i].isAlive) continue;
-              if (i == now.num) continue;
-              if (!visited_others[i]) continue;
-
-              ans += now.size * groups[i].size;
-            }
-
-            found = true;
-            break;
           }
+
+          // 인접한 군집끼리 계산
+          for (int i = 1; i < groups.size(); ++i) {
+            if (!groups[i].isAlive) continue;
+            if (i == now.num) continue;
+            if (!visited_others[i]) continue;
+
+            ans += now.size * groups[i].size;
+          }
+
+          found = true;
+          break;
         }
       }
+    }
 
-      if (!found) now.isAlive = false;
-    }
-    // DB
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        cout << MAP[i][j] << ' ';
-      }
-      cout << '\n';
-    }
-    // DB
-    cout << ans << '\n';
+    if (!found) now.isAlive = false;
   }
+  // cout << '\n';
 
-  system("pause");
-  return 0;
+
+  // DB
+  // for (int i = 0; i < n; ++i) {
+  //   for (int j = 0; j < n; ++j) {
+  //     cout << MAP[i][j] << ' ';
+  //   }
+  //   cout << '\n';
+  // }
+  // DB
+  cout << ans << '\n';
+}
+
+// system("pause");
+return 0;
 }
