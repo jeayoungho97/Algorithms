b97f78983d44e05f84b5d586dc77c3914b59353c
jeayoungho97 <jeayh97@naver.com>
2025-09-23 14:06:38 +0900

    [change] 주석추가, 수정

--- CODETREE/queen_ant.cpp
+++ CODETREE/queen_ant.cpp
@@ -1,3 +1,11 @@
+// 핵심 아이디어 : 구간의 합의 최댓값에 대해 이분탐색
+//  예) 최댓값 100 => 살아있는 첫 개미집부터 거리 재다가 100을 넘으면 새로운 개미가 거기서부터 출발
+//                => 반복하다보면 필요한 개미의 수가 나옴
+//                => 주어진 개미의 수보다 많다면 최댓값을 넉넉하게 잡음
+//                => 주어진 개미의 수보다 적거나 같다면 해당 값을 저장해놓고 최댓값을 작게도 시도해봄
+
+// 이분탐색 시 주의점 : 주어진 개미의 수와 딱 맞지 않을 수 있음미
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -22,7 +30,7 @@ int check(int val) {
   }
 
   int cnt = 1;
-  for (i + 1; i < vsize; ++i) {
+  for (++i; i < vsize; ++i) {
     if (!v[i].isAlive) continue;
 
     if (v[i].num - start > val) {
@@ -46,8 +54,6 @@ long long BS(int r) {
 
   while (start <= end) {
     long long mid = (start + end) / 2;
-    // cout << "start : " << start << " end : " << end << " mid : " << mid << '\n';
-
     int cnt = check(mid);
     if (cnt <= r) {
       ret = mid;
@@ -57,8 +63,6 @@ long long BS(int r) {
       start = mid + 1;
     }
   }
-
-  // cout << "result : " << ret << '\n';
   return ret;
 }
 
@@ -76,7 +80,6 @@ int main() {
       cin >> N;
       alive = N;
       for (int i = 1; i <= N; ++i) {
-        
         int dist;
         cin >> dist;
         v.push_back({dist, true});
