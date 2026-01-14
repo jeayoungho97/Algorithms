c2f524e22d590b1ad80e416852ee0514a6f945e8
Youngho Jea <jeayh97@naver.com>
2026-01-14 09:32:05 +0900

    [modified] 람다함수 및 비교 연산자 구조체로 정의

--- PRIORITY_QUEUE/double_pq.cpp
+++ PRIORITY_QUEUE/double_pq.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
 struct Node {
-    long long num, idx;
+    int val, idx;
+};
 
-    bool operator<(const Node& rhs) const {
-        if (num == rhs.num) return idx < rhs.idx;
-        return num < rhs.num;
+struct MaxCmp {
+    bool operator()(const Node& a, const Node& b) const {
+        if (a.val != b.val) return a.val < b.val;
+        return a.idx < b.idx;
     }
 };
 
-bool visited[1000001];
-
-priority_queue<Node> pq;
-priority_queue<Node> rpq;
+struct MinCmp {
+    bool operator()(const Node& a, const Node& b) const {
+        if (a.val != b.val) return a.val > b.val;
+        return a.idx > b.idx;
+    }
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -25,48 +29,58 @@ int main() {
     cin >> T;
 
     while (T--) {
-        fill(visited, visited + 1000001, false);
-        while (!pq.empty()) pq.pop();
-        while (!rpq.empty()) rpq.pop();
+        int k;
+        cin >> k;
 
-        int num, cnt = 0;
-        cin >> num;
-        for (int i = 0; i < num; ++i) {
-            char ch; long long val;
-            cin >> ch >> val;
+        vector<char> visited(k + 1, 0);
 
-            if (ch == 'I') {
-                pq.push({val, cnt});
-                rpq.push({-val, cnt});
-                cnt++;
-            }
-            else {
-                if (val == 1) {
-                    while (!pq.empty() && visited[pq.top().idx]) pq.pop();
-                    if (!pq.empty()) {
-                        visited[pq.top().idx] = true;
-                        pq.pop();
+        priority_queue<Node, vector<Node>, MaxCmp> maxpq;
+        priority_queue<Node, vector<Node>, MinCmp> minpq;
+
+        int idx = 0;
+
+        auto cleanMax = [&]() {
+            while (!maxpq.empty() && visited[maxpq.top().idx]) maxpq.pop();
+        };
+        auto cleanMin = [&]() {
+            while (!minpq.empty() && visited[minpq.top().idx]) minpq.pop();
+        };
+
+        for (int i = 0; i < k; ++i) {
+            char op;
+            int x;
+            cin >> op >> x;
+
+            if (op == 'I') {
+                maxpq.push({x, idx});
+                minpq.push({x, idx});
+                idx++;
+            } else {
+                if (x == 1) {
+                    cleanMax();
+                    if (!maxpq.empty()) {
+                        visited[maxpq.top().idx] = 1;
+                        maxpq.pop();
                     }
-                }
-                
-                if (val == -1) {
-                    while (!rpq.empty() && visited[rpq.top().idx]) rpq.pop();
-                    if (!rpq.empty()) {
-                        visited[rpq.top().idx] = true;
-                        rpq.pop();
+                } else {
+                    cleanMin();
+                    if (!minpq.empty()) {
+                        visited[minpq.top().idx] = 1;
+                        minpq.pop();
                     }
                 }
             }
         }
-        while (!pq.empty() && visited[pq.top().idx]) pq.pop();
-        while (!rpq.empty() && visited[rpq.top().idx]) rpq.pop();
 
-        if (pq.empty() || rpq.empty()) cout << "EMPTY\n";
-        else {
-            cout << pq.top().num << ' ' << -(rpq.top().num) << '\n';
+        cleanMax();
+        cleanMin();
+
+        if (maxpq.empty() || minpq.empty()) {
+            cout << "EMPTY\n";
+        } else {
+            cout << maxpq.top().val << ' ' << minpq.top().val << '\n';
         }
     }
-
 
     return 0;
 }
