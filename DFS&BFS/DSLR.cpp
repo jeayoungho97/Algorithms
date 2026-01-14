7fba772a24bfc5f5bc5e1eb6f2c5908960db803c
Youngho Jea <jeayh97@naver.com>
2026-01-14 19:53:04 +0900

    [modified] stamp 방식 + 배열 큐 + 배열 역추적 (GPT 수정본)

--- DFS&BFS/DSLR.cpp
+++ DFS&BFS/DSLR.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
+#include <cstring>
 using namespace std;
+
+static int prevv[10000];
+static char how[10000];
+static int vis[10000];
+static int stamp = 1;
+
+static int q[10000];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -15,61 +20,69 @@ int main() {
         int A, B;
         cin >> A >> B;
 
-        vector<int> prev(10000, -1);
-        vector<char> how(10000, 0);
-        vector<bool> visited(10000, 0);
+        int head = 0, tail = 0;
+        q[tail++] = A;
 
-        queue<int> q;
-        q.push(A);
-        visited[A] = 1;
+        stamp++;
+        if (stamp == INT_MAX) { // 혹시 모를 오버플로 방지(실제로는 거의 안 옴)
+            memset(vis, 0, sizeof(vis));
+            stamp = 1;
+        }
 
-        while (!q.empty()) {
-            int cur = q.front(); q.pop();
+        vis[A] = stamp;
+        prevv[A] = -1;
+
+        while (head < tail) {
+            int cur = q[head++];
             if (cur == B) break;
 
             // D
-            int nxt = (cur * 2) % 10000;
-            if (!visited[nxt]) {
-                visited[nxt] = 1;
-                prev[nxt] = cur;
+            int nxt = (cur << 1) % 10000;
+            if (vis[nxt] != stamp) {
+                vis[nxt] = stamp;
+                prevv[nxt] = cur;
                 how[nxt] = 'D';
-                q.push(nxt);
+                q[tail++] = nxt;
             }
 
             // S
-            nxt = (cur == 0 ? 9999 : cur - 1);
-            if (!visited[nxt]) {
-                visited[nxt] = 1;
-                prev[nxt] = cur;
+            nxt = (cur == 0) ? 9999 : (cur - 1);
+            if (vis[nxt] != stamp) {
+                vis[nxt] = stamp;
+                prevv[nxt] = cur;
                 how[nxt] = 'S';
-                q.push(nxt);
+                q[tail++] = nxt;
             }
 
             // L
             nxt = (cur % 1000) * 10 + (cur / 1000);
-            if (!visited[nxt]) {
-                visited[nxt] = 1;
-                prev[nxt] = cur;
+            if (vis[nxt] != stamp) {
+                vis[nxt] = stamp;
+                prevv[nxt] = cur;
                 how[nxt] = 'L';
-                q.push(nxt);
+                q[tail++] = nxt;
             }
 
             // R
             nxt = (cur / 10) + (cur % 10) * 1000;
-            if (!visited[nxt]) {
-                visited[nxt] = 1;
-                prev[nxt] = cur;
+            if (vis[nxt] != stamp) {
+                vis[nxt] = stamp;
+                prevv[nxt] = cur;
                 how[nxt] = 'R';
-                q.push(nxt);
+                q[tail++] = nxt;
             }
         }
 
-        string ans;
-        for (int x = B; x != A; x = prev[x]) ans.push_back(how[x]);
-        reverse(ans.begin(), ans.end());
-        cout << ans << '\n';
+        // 역추적: B -> A
+        char out[10000];
+        int len = 0;
+        for (int x = B; x != A; x = prevv[x]) {
+            out[len++] = how[x];
+        }
+        // 뒤집어서 출력
+        for (int i = len - 1; i >= 0; --i) cout << out[i];
+        cout << '\n';
     }
-
 
     return 0;
 }
